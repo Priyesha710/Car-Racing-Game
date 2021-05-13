54b46486a8665b3f5a34dda07c5271a24cb513c2
Priyesha710 <73739950+Priyesha710@users.noreply.github.com>
2021-05-13 13:45:34 +0530

    Update Form.js

--- userDefinedClasses/Form.js
+++ userDefinedClasses/Form.js
@@ -86,7 +86,7 @@ the form object.
       to zero in the program and in the database. */
       gameObj.updateState(0);
       playerObj.updateCount(0);
-        playerObj.deletePlayersInfo();
+        Player.deletePlayersInfo();
         Player.updateCarsAtEnd(0);
     });
   }
